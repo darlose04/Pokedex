2ff68996cfa6fea9f286794fb1d5e0016aa4277a
Zach Smith <zsmith126028@gmail.com>
2019-05-26 20:06:59 -0500

    add new div and classname

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Pokecard from './Pokecard';
-import './Pokedex';
+import './Pokedex.css';
 
 class Pokedex extends Component {
   static defaultProps = {
@@ -20,14 +20,17 @@ class Pokedex extends Component {
     return (
       <div className="Pokedex">
         <h1>Pokedex!</h1>
-        {this.props.pokemon.map((p) => (
-          <Pokecard 
-            id={p.id}
-            name={p.name}
-            type={p.type}
-            exp={p.base_experience}
-          />
-        ))}
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard 
+              key={p.id}
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
       </div>
     )
   }
