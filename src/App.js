ea47ebb10fbeae855f7e4d53a7eb6d6e5bf075bf
Zach Smith <zsmith126028@gmail.com>
2019-05-26 19:52:17 -0500

    import Pokedex component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Pokecard from './components/Pokecard';
+import Pokedex from './components/Pokedex';
 
 function App() {
   return (
     <div className="App">
-      <Pokecard 
-        id={4} 
-        name='Charmander' 
-        type='fire'
-        exp={62}
-      />
+      <Pokedex />
     </div>
   );
 }
